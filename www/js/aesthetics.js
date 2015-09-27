f70f7f395a2eca378968bf45866f06e90c12631a
Hifza Sakhi <hifzasakhi@gmail.com>
2015-09-26 20:48:12 -0400

    made the background change with selection for the X, Y, Z coordinates

--- www/js/aesthetics.js
+++ www/js/aesthetics.js
@@ -3,14 +3,26 @@ $( document ).ready(function() {
     $(".movement").click(function(e) {
 
     	$elem = $(e.currentTarget);
-    	
     	$elem.addClass('teal')
 
     	$('.teal').each(function() {
-		    $(this).removeClass('teal') 
+    		if($(this) != $elem) {
+    			$(this).removeClass('teal') 
+    		}
+		    
+		});
+		$elem.toggleClass('teal') 	
+    });
+
+    $(".legendCoord").click(function(e) {
+
+    	$elem = $(e.currentTarget);
+
+    	$('.tealLegend').each(function() {
+		    $(this).removeClass('tealLegend') 
 		});
 		
-		$elem.toggleClass('teal') 	
+		$elem.toggleClass('tealLegend') 	
         
     });
 });
